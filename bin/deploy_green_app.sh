3bf0a2f400e0e5bc9ea38f8a7c9d541abc67674f
Plaw <plj611@outlook.com>
2021-09-04 18:10:32 +0800

    add k8s rollout health check

--- bin/deploy_green_app.sh
+++ bin/deploy_green_app.sh
@@ -2,3 +2,4 @@
 
 . ./config.txt
 kubectl create deployment get_ip_${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --replicas=2 --port=80
+kubectl rollout status deployment/get_ip_${CURRENT} # Health check
