a3771b3fd1f3f49c9d15981ce35f329b9083bd5b
Plaw <plj611@outlook.com>
2021-09-04 18:43:48 +0800

    change object name

--- bin/deploy_green_app.sh
+++ bin/deploy_green_app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 . ./config.txt
-kubectl create deployment get_ip_${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --replicas=2 --port=80
-kubectl rollout status deployment/get_ip_${CURRENT} # Health check
+kubectl create deployment get-ip-${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --replicas=2 --port=80
+kubectl rollout status deployment/get-ip-${CURRENT} # Health check
